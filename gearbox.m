1357a5075b9dba5e9262f1003b689f3459b3a90c
paulmolloy <molloyp1@tcd.ie>
2018-11-27 16:01:59 +0000

    Add surf view and extra test inputs, change gear member variable.

--- gearbox.m
+++ gearbox.m
@@ -8,9 +8,9 @@ fis = addMF(fis,"fluid_temp","trapmf",[90 110 150 150],'Name',"high");
 fis = addInput(fis, [0, 150], "Name", "speed");
 fis = addMF(fis,"speed","gaussmf",[30 0],'Name',"very slow");
 fis = addMF(fis,"speed","gaussmf",[30 30],'Name',"slow");
-fis = addMF(fis,"speed","gaussmf",[30 75],'Name',"moderate");
-fis = addMF(fis,"speed","gaussmf",[30 120],'Name',"fast");
-fis = addMF(fis,"speed","gaussmf",[30 150],'Name',"very fast");
+fis = addMF(fis,"speed","gaussmf",[30 60],'Name',"moderate");
+fis = addMF(fis,"speed","gaussmf",[30 100],'Name',"fast");
+fis = addMF(fis,"speed","gaussmf",[40 150],'Name',"very fast");
 
 fis = addInput(fis, [0, 5], "Name", "throttle"); % Volts
 fis = addMF(fis,"throttle","gaussmf",[1 5],'Name',"open");
@@ -19,15 +19,15 @@ fis = addMF(fis,"throttle","gaussmf",[1 0],'Name',"closed");
 
 fis = addInput(fis, [0, 1], "Name", "friction");
 fis = addMF(fis,"friction","gaussmf",[.3 0],'Name',"slippy");
-fis = addMF(fis,"friction","gaussmf",[.6 .5],'Name',"stable");
+fis = addMF(fis,"friction","gaussmf",[.3 .5],'Name',"stable");
 fis = addMF(fis,"friction","gaussmf",[.3 1],'Name',"rough");
 
-fis = addOutput(fis,[1 5],'Name',"gear");
-fis = addMF(fis,"gear","trimf",[1 1 1.5],'Name',"first");
+fis = addOutput(fis,[0.5 5.5],'Name',"gear");
+fis = addMF(fis,"gear","trapmf",[0 0 1 1.5],'Name',"first");
 fis = addMF(fis,"gear","trimf",[1.5 2 2.5],'Name',"second");
 fis = addMF(fis,"gear","trimf",[2.5 3 3.5],'Name',"third");
 fis = addMF(fis,"gear","trimf",[3.5 4 4.5],'Name',"fourth");
-fis = addMF(fis,"gear","trimf",[4.5 5 6],'Name',"fifth");
+fis = addMF(fis,"gear","trapmf",[4.5 5  6 6],'Name',"fifth");
 
 
 % (fluid temp) param 1 idx of membership function,
@@ -38,20 +38,25 @@ fis = addMF(fis,"gear","trimf",[4.5 5 6],'Name',"fifth");
 % rule wright 0 or 1,
 % fuzzy operator 1==AND 2==OR
 % TODO: Add more rules.
-rulesList = [2 1 0 0 2 1 1;  % if very_slow && high temp -> second
-             1 1 0 0 1 1 1;  % if very_slow && normal temp-≥ first
+rulesList = [1 1 0 0 1 1 1;  % if very_slow && normal temp-≥ first
+             2 1 0 0 2 1 1;  % if very_slow && high temp -> second
              0 2 0 0 2 1 1; % if slow -> second
              0 5 0 0 5 1 1]; % if very_fast -> fifth
 fis = addRule(fis, rulesList);
 
 fis.output
-plotmf(fis,"input",2)
+plotmf(fis,"output",1)
 
-inputs = [60 15 4.5 .78; % normal temp, very slow, throttle open, rough
-          102 15 4.5 .78; % high temp, very slow, throttle open, rough
+        % Temp  Speed  Throttle   friction 
+inputs = [
+          1 1 0 0;
+          40 1 0 0;
+          40 1 4.5 .78; % normal temp, very slow, throttle open, rough
+          120 15 4.5 .78; % high temp, very slow, throttle open, rough
           60 50 4.5 .78; % normal temp, slow, throttle open, rough
           60 140 4.5 .78; % normal temp, very fast, throttle open, rough
     ]
 results = evalfis(fis, inputs)
 rounded_gear = round(results)
-plotfis(fis)
+%plotfis(fis)
+surfview(fis)
